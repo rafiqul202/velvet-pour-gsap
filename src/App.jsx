32f073b6172ee923c1f0c3d45b4b19fe1c7e4b84
Rafiqul Hasan <rafiqulhasan398@gmail.com>
2025-07-05 16:42:49 +0600

    valvet pour gsap animation header section completed

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import gsap from "gsap";
+import "./App.css";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black"></div>
+    </main>
+  );
+}
+
+export default App;
